09d3d1ffbed2afa4fe800312b1bb14aea1802e1e
yangbin <yangbin@weidian.com>
2017-08-29 19:31:53 +0800

    开启协程 封装Glide

---
+++ app/src/main/java/com/binlly/fastpeak/base/glide/Glide.kt
@@ -0,0 +1,12 @@
+package com.binlly.fastpeak.base.glide
+
+import android.content.Context
+
+/**
+ * Created by yy on 2017/8/29.
+ */
+object Glide {
+    fun displayLocalImage(context: Context) {
+
+    }
+}
